340f6cae2dedb51efefd9587b8a57ac8322f5490
6 0 <aoin_jyar@yahoo.co.jp>
2014-10-09 18:51:45 +0900

    コミット3回目

--- GitTest/GitTest.cpp
+++ GitTest/GitTest.cpp
@@ -10,6 +10,8 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Hello,World!" << endl;
 	cout << "Goodnight,World!(追加行)" << endl;
+	cout << "Good morning,World!(分岐)" << endl;
+	cout << "ネタ切れ(bunki2編集)" << endl;
 	getchar();
 	return 0;
 }
