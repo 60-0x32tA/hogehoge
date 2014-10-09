a2adf130bc29f53f9829ed7c984919826a6e7dd2
6 0 <aoin_jyar@yahoo.co.jp>
2014-10-09 18:16:27 +0900

    コミット1回目

---
+++ GitTest/GitTest.cpp
@@ -0,0 +1,15 @@
+// GitTest.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello,World!" << endl;
+	getchar();
+	return 0;
+}
+
